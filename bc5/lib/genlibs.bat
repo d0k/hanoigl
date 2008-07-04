e930c37bf1c4e3ea3eb56fb7125d85453974f5f3
Benjamin Kramer <benny.kra@gmail.com>
2008-07-04 15:48:12 +0200

    replace old buildsystem with CMake

--- bc5/lib/genlibs.bat
+++
@@ -1,7 +0,0 @@
-REM Generates .lib files for Borland C++
-
-implib opengl32.lib %SYSTEMROOT%\system32\opengl32.dll
-implib glu32.lib %SYSTEMROOT%\system32\glu32.dll
-implib winmm.lib %SYSTEMROOT%\system32\winmm.dll
-
-implib glut32.lib ..\glut32.dll
